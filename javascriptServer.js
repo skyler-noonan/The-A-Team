c13b3c6c231a768ff7aaebad50eb0c4b098a278a
Drexler Nastor <drexler@Drexlers-MacBook-Pro.local>
2022-12-03 07:10:58 -0500

    D

--- javascriptServer.js
+++ javascriptServer.js
@@ -1,39 +1,8 @@
-/*var profileUsername = "A-Team Username"; 
-var serverPassword = ""; 
-var serverEmail = ""; 
-var loginUsername = "";
-var loginPassword = "";
-var loginEmail = "";
-
-var profilePic = "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg";
-
-const express = require('express');
-const app = express();
+var express=require('express');
+var app=express();
 app.listen(3000, () => console.log('listening at 3000'));
-*/
-var url = "http://localhost:3000/post"
-var port = 3000 
-
-
-
-app.post('/post', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    var z = JSON.parse(req.query['data']);
-
-
-
-    if (z['action'] == 'upload'){
-        console.log(z.uploadDescription)
-       
-        
-        var jsontext = JSON.stringify({
-            'action': 'generateCode',
-            'nameID': nameID,
-            'msg': 'New code generated!!!'
-        });
-        console.log(jsontext);
-        console.log(codes);
-        // send the response while including the JSON text	*/	
-        
-        res.send(jsontext)
-    }}).listen(3000) 
+app.get('/',function(req,res)
+{
+res.send('Hello World!');
+});
+var server=app.listen(3000,function() {});
