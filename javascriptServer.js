8681dcf163a73b0d28063ab5c6424beb6a9e841f
Drexler Nastor <drexler@Drexlers-MacBook-Pro.local>
2022-12-03 07:51:20 -0500

    D

--- javascriptServer.js
+++ javascriptServer.js
@@ -1,10 +1,10 @@
 var express=require('express');
 var app=express();
-const port = 3000
+const port = 80
 app.listen(port, () => console.log('listening at 3000'));
 app.get('/',function(req,res)
 {   
     res.send('Hello World!');
 
 });
-var server=app.listen(3000,function() {});
+var server=app.listen(port,function() {});
