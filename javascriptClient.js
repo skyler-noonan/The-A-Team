072daae1b27e40604c46cb8455036a282a3c9a06
Drexler Nastor <drexler@Drexlers-MacBook-Pro.local>
2022-12-02 19:16:53 -0500

    d

--- javascriptClient.js
+++ javascriptClient.js
@@ -300,13 +300,14 @@ function generateOtherUserPost(numPost) {
             $(posterInfo).attr("id", "posterInfo");
                 span3 = document.createElement("p"); //username
                 console.log("create p")
-                $(posterInfo).append(span3);
+               
                 $(span3).attr("id", "username" +numPost);
+                $(posterInfo).append("#username"+numPost);
                 $("#username"+numPost).click(function(){
                     window.open("./userprofile.html")
                     window.close("./homepage.html") 
                 })
-                $("#username"+numPost).html(generateUsername());
+                $("#username"+numPost).html("test");
                 $("#username"+numPost).css("font-weight", "bold");
                 
 
