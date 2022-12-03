623cd4842b571256e46d15559eb7448a74e85a66
TristanR1 <tristan0@my.yorku.ca>
2022-12-03 06:23:59 -0500

    adding tests

--- TEST.js
+++ TEST.js
@@ -1,15 +1,49 @@
-//test cases 1012ab22f
+//test cases 
 assert = chai.assert;
 
 // describe() is a function by which you can define a collection of tests. 
 // It takes two parameters, the 1st one is a description of what is being tested, and 
-// the 2nd one is a function which contains one or multiple tests each defined by one it().
-describe('Testing function generateUsername()', function () {
+describe('Testing Functionality', function () {
  
 
-  it('Test 2: the returned value is from type string', () => {
+  it('Test: These functions return string', () => {
     assert.typeOf(generateUsername(), 'string');
+    assert.typeOf(generateDescription(), 'string');
+    assert.typeOf(generateNotifications(), 'string');
+    assert.typeOf(generateHashtag(), 'string');
+    assert.typeOf(generateProfilePicture(), 'string');
+    assert.typeOf(generatePostPicture(), 'string');
+    assert.typeOf(generateComment(), 'string');
+  
+
+
+
+
+
+
+
 });
+
+it('Test: These functions\' returned value is < 1001', () => {
+    for (var i = 1; i <= 10000; i++) {
+        assert(generateNumLikes() < 1001);
+        assert(generateNumFollowers() < 1001);
+        assert(generateNumFollowing() < 1001);
+
+        
+    }
+});
+
+it('Test: These functions return number', () => {
+    assert.typeOf(generateNumLikes(), 'number');
+    assert.typeOf(generateNumFollowers(), 'number');
+    assert.typeOf(generateNumFollowing(), 'number');
+
+});
+
+
+
+
   
  
 })
